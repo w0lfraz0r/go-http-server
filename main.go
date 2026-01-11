86964963953469ce1a7d53115101be36b909d53b
vaibhav0shrivas <vaibhavshrivasone@gmail.com>
2026-01-11 17:36:21 +0530

    reading 8 bytes and printing

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Println("I hope I get the job!")
+	file, err := os.Open("messages.txt") // For read access.
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	// Further file operations would go here.
+
+	data := make([]byte, 8)
+	for {
+		count, err := file.Read(data)
+		if err != nil {
+			if err.Error() == "EOF" {
+				break
+			}
+			log.Fatal(err)
+		}
+		fmt.Printf("read: %s\n", data[:count])
+	}
 }
